6b5ee377f54e4d5e762621bcf55402d2eea19f84
Damjan Savic <info@damjan-savic.com>
2025-04-15 21:03:34 +0200

    Fix - 15.04.2025.

--- src/pages/references/index.tsx
+++ src/pages/references/index.tsx
@@ -1,8 +1,16 @@
 // src/pages/references/index.tsx
 import { Hero } from "./components/Hero";
 import { CategoriesSection } from "./components/Categories";
+import { FeaturedProjectsSection } from "./components/FeaturedProjects";
 import { ProcessSection } from "./components/Process";
 import { ClientFeedbackSection } from "./components/ClientFeedback";
 import { CTASection } from "./components/CTASection";
 
-export { Hero, CategoriesSection, ProcessSection, ClientFeedbackSection, CTASection };
+export {
+    Hero,
+    CategoriesSection,
+    FeaturedProjectsSection,
+    ProcessSection,
+    ClientFeedbackSection,
+    CTASection
+};
