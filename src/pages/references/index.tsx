2ed68c20b5c678fa4059e29e048acf1c840cc265
Damjan Savic <info@damjan-savic.com>
2025-04-15 21:24:57 +0200

    Fix - 15.04.2025.

--- src/pages/references/index.tsx
+++ src/pages/references/index.tsx
@@ -1,17 +1,8 @@
 // src/pages/references/index.tsx
-import { Hero } from "./components/Hero";
-import { CategoriesSection } from "./components/Categories";
-import { FeaturedProjectsSection } from "./components/FeaturedProjects";
-import { ProcessSection } from "./components/Process";
-import { ClientFeedbackSection } from "./components/ClientFeedback";
-import { CTASection } from "./components/CTASection";
-
-// Explizite named exports
-export {
-    Hero,
-    CategoriesSection,
-    FeaturedProjectsSection,
-    ProcessSection,
-    ClientFeedbackSection,
-    CTASection
-};
+// Direkte Re-Exports, um sicherzustellen, dass alle Komponenten korrekt exportiert werden
+export { Hero } from "./components/Hero";
+export { CategoriesSection } from "./components/Categories";
+export { FeaturedProjectsSection } from "./components/FeaturedProjects";
+export { ProcessSection } from "./components/Process";
+export { ClientFeedbackSection } from "./components/ClientFeedback";
+export { CTASection } from "./components/CTASection";
