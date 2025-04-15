a60c84113f12dbed113ed1fcabd5c8724a71c675
Damjan Savic <77978776+damjan1996@users.noreply.github.com>
2025-04-16 00:00:01 +0200

    Update brevo-test.js

--- brevo-test.js
+++ brevo-test.js
@@ -5,7 +5,7 @@ import SibApiV3Sdk from 'sib-api-v3-sdk';
 async function testBrevoAPI() {
     try {
         // API-Schlüssel direkt einsetzen für den Test (nur zu Testzwecken, nicht in Produktion)
-        const API_KEY = "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";
+        const API_KEY = process.env.BREVO_API_KEY;
 
         // Initialize Brevo API client
         const defaultClient = SibApiV3Sdk.ApiClient.instance;
