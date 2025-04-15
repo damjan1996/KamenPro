2417a868c68f979e3740dfdf9f5b2eb7f984d0b9
Damjan Savic <info@damjan-savic.com>
2025-04-15 21:26:43 +0200

    Fix - 15.04.2025.

--- src/pages/References.tsx
+++ src/pages/References.tsx
@@ -1,14 +1,12 @@
 // src/pages/References.tsx
 import { Seo } from "../components/Seo";
-// Direkter Import der Komponenten ohne Dateiendung
-import {
-    Hero,
-    CategoriesSection,
-    FeaturedProjectsSection,
-    ProcessSection,
-    ClientFeedbackSection,
-    CTASection
-} from "./references/index";
+// Direkter Import der Komponenten aus ihren Dateien
+import { Hero } from "./references/components/Hero";
+import { CategoriesSection } from "./references/components/Categories";
+import { FeaturedProjectsSection } from "./references/components/FeaturedProjects";
+import { ProcessSection } from "./references/components/Process";
+import { ClientFeedbackSection } from "./references/components/ClientFeedback";
+import { CTASection } from "./references/components/CTASection";
 
 export default function References() {
     return (
